c9e86e16adbdc88ffb52af404f2f2aa6dec64cf8
PK <pradeepdhital124@gmail.com>
2023-04-06 20:23:58 +1000

    basic layout is done except for the logic applied

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Home } from "./components/Home";
 import { Navigation } from "./components/Navigation";
 import "./style.css";
+import { Calendar } from "./components/Calendar";
+import { Gallery } from "./components/Gallery";
+import { FAQ } from "./components/FAQ";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
 function App() {
   return (
     <div>
@@ -11,6 +16,11 @@ function App() {
           <Navigation></Navigation>
           <div className="mainbody">
             <Home></Home>
+            <Calendar></Calendar>
+            <Gallery></Gallery>
+            <FAQ></FAQ>
+            <About></About>
+            <Contact></Contact>
           </div>
         </div>
       </div>
