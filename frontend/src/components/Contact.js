38ddfa4fe3fbd9d9261104442f3e66bf87e2a71e
PK <pradeepdhital124@gmail.com>
2023-04-06 20:59:57 +1000

    working on footer basic set up

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="contact">
+      <div className="contact_div">
+        <h1 className="contact_heading">
+          Interested on keeping upto date with other DSQ information and
+          services?
+        </h1>
+      </div>
+    </div>
+  );
+};
